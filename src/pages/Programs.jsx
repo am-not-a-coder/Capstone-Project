c254308c0a7133e75b8dcb3cd1d3726cba0c3116
Caparic Rafael <rafaelcaparic@gmail.com>
2025-06-15 17:09:57 +0800

    start

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -4,7 +4,7 @@
 const Programs = () => {
     return(
         <main  className="flex-1 p-4 h-full col-span-4 row-span-4 col-start-2 row-start-2 overflow-y-auto">
-
+            <div className="program-card"></div>
         </main>
     )
 };
