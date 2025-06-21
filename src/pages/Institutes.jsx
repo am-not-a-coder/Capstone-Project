a52ada4f483c6e19e0c7461057cc313b472cbb92
Caparic Rafael <rafaelcaparic@gmail.com>
2025-06-21 18:02:28 +0800

    institute starting point

--- src/pages/Institutes.jsx
+++ src/pages/Institutes.jsx
@@ -1,11 +1,34 @@
 //for imports
-
+import { useState } from "react";
 
 const Institutes = () => {
-    
+
+        {/*use state function*/}
+        const [institutes, setinstitutes] = useState([
+          {
+            code: "CCS",
+            name: "College of Computing Studies",
+            img: "#FFA500",
+            instituteDean: "John Doe",
+          },
+          {
+            code: "CED",
+            name: "College of Education",
+            img: "#FF0000",
+            instituteDean: "John Joe",
+          },
+          {
+            code: "CAS",
+            name: "College of Arts and Sciences",
+            img: "#FFFF00",
+            instituteDean: "John Boe",
+          },
+
+        ])
+        
     return(
         <main  className="flex-1 p-4 h-full col-span-4 row-span-4 col-start-2 row-start-2 overflow-y-auto">
-
+            
         </main>
     )
 };
