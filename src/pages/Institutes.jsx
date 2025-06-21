ec797aa8b43b390709d57d920721bcdf918e4366
Caparic Rafael <rafaelcaparic@gmail.com>
2025-06-21 17:32:28 +0800

    create a reuseable card for program and institute

--- src/pages/Institutes.jsx
+++ src/pages/Institutes.jsx
@@ -4,7 +4,7 @@
 const Institutes = () => {
     return(
         <main  className="flex-1 p-4 h-full col-span-4 row-span-4 col-start-2 row-start-2 overflow-y-auto">
-
+            
         </main>
     )
 };
