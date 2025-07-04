e03ef7ae29d5c31b870cf85163c6e3de5abe1f78
JC <jcabad1027@gmail.com>
2025-07-05 00:09:00 +0800

    Implement Login Validation (server and client-side)

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,30 +2,36 @@ from flask import jsonify, request, session
 from app.models import User
 from app import db
 from werkzeug.security import check_password_hash
+from app.decorator import login_required
 
 def register_routes(app):
-    @app.route('/api/ping', methods=['GET'])
-    def ping():
+    @app.route('/api/checkdb', methods=['GET'])
+    def checkdb():
         return jsonify({'message': "The backend is working"})
     
     @app.route('/api/users', methods=['GET'])
+    @login_required
     def get_users():
         users = User.query.all()
         return jsonify({'employeeID': u.employeeID, 'password': u.password} for u in users)
     
     @app.route('/api/login', methods=["POST"])
+    @login_required
     def login():
+        #Get the JSON from the request
         data = request.get_json()
-        empID = data["employeeID"]
-        password = data["password"]
+        empID = data.get("employeeID")
+        password = data.get("password")
+        #Fetches the user from the db using the employeeID
         user = User.query.filter_by(employeeID=empID).first()
-
+        #If the user is not found
         if user is None:
-            return jsonify({'message': 'User not found'})
+            return jsonify({'success': False, 'message': 'User not found'})
         
         elif check_password_hash(user.password, password): #checks if the user input hashed password matches the hashed pass in the db 
+            # After login, store the user info in the session
             session["employeeID"] = user.employeeID
-            session["name"] = user.fName + user.lName + user.suffix
+            session["name"] = str(user.fName) + str(user.lName) + str(user.suffix)
             session["email"] = user.email
             session["contactNum"] = user.contactNum
             session["profilePic"] = user.profilePic
@@ -33,13 +39,15 @@ def register_routes(app):
             session["isOnline"] = user.isOnline
             user.isOnline = True
             db.session.commit()
-            return jsonify({'success': True, 'message': 'The password matched'})
+            return jsonify({'success': True, 'message': 'Login successful'})
             
         else:
-            return jsonify({'success': False, 'message': "The password didn't matched"})
+            return jsonify({'success': False,'message': "Invalid password"})
         
     @app.route('/api/logout', methods=["POST"])
     def logout():
-        isOnline = False
+        session["isOnline"] = False
         session.clear()
         return jsonify({"message":"You have logged out the system"})
+    
+    
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios  from 'axios';
 import Carousel from '../components/Carousel';
 import udmsLogo from '../assets/udms-logo.png';
 import UDMLogo from '../assets/UDM-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faKey,
-    faUser
+    faUser,
+    faEye,
+    faEyeSlash
 } from '@fortawesome/free-solid-svg-icons';
 
 
 const Login = () => {
+
+    const [employeeID, setEmployeeID] = useState('');
+    const [error, setError] = useState(null);
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState();
+
+    const navigate = useNavigate()
+
+    const handleLogin = async (e) =>{
+        e.preventDefault();
+
+        if(!employeeID.trim() || !password.trim()){
+            setError('Please enter a valid employee ID and password')
+            return;
+        }
+
+        try {
+            const response = await axios.post('/api/login', {employeeID, password}, {withCredentials: true});
+            if (response.data.success) {
+                setError(null);
+                navigate('/Dashboard');
+                localStorage.setItem('isLoggedIn', 'true')
+            } else {
+                setError(response.data.message);
+            }
+        } catch (err) {
+            setError('Server error. Please try again.');
+        }
+    }
+
     return(
     <>
     {/* container w/ gradient-background */}
@@ -35,28 +69,44 @@ const Login = () => {
                     </div>
                 {/* form */}
 
-                <form className='flex flex-col flex-1 px-8 py-5 rounded-xl shadow-3xl text-neutral-800'>
+                <form onSubmit={handleLogin} className='flex flex-col flex-1 px-8 py-5 rounded-xl shadow-3xl text-neutral-800'>
                 
              
                     <h2 className='mb-6 text-3xl font-bold text-center text-neutral-800'>Login</h2>
-                    <div className='relative mb-4'>
-                        <label htmlFor='employeeID' className='block text-sm font-medium text-gray-700'>Employee ID</label>
-                        <FontAwesomeIcon icon={faUser} className='absolute top-9.5 left-2 text-md text-gray-400' />
-                        <div className='absolute top-8.5 left-8 h-6 border-l border-gray-400'></div>
-                        <input type='text' id='employeeID' placeholder="Employee ID" className='block w-full px-10 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-zuccini-900 focus:border-zuccini-900' />
-                    </div>
-                    <div className='relative mb-6'>
-                        <label htmlFor='password' className='block text-sm font-medium text-gray-700'>Password</label>
-                        <FontAwesomeIcon icon={faKey} className='absolute top-9.5 left-2 text-md text-gray-400' />
-                        <div className='absolute top-8.5 left-8 h-6 border-l border-gray-400'></div>
-                        <input type='password' id='password' placeholder="Password" className='block w-full px-10 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-zuccini-900 focus:border-zuccini-900' />
-                    </div>
+                 
+                        <div className='relative mb-4'>
+                            <label htmlFor='employeeID' className='block text-sm font-medium text-gray-700'>Employee ID</label>
+                            <FontAwesomeIcon icon={faUser} className='absolute top-9.5 left-2 text-md text-gray-400' />
+                            <div className='absolute top-8.5 left-8 h-6 border-l border-gray-400'></div>
+                            <input type='text' 
+                            name='employeeID' 
+                            placeholder="Employee ID"
+                            value={employeeID}
+                            onChange={(e) => setEmployeeID(e.target.value)}
+                            className={`block w-full px-10 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none ${error ? 'border-red-600' : ''} focus:ring-zuccini-900 focus:border-zuccini-900`}/>
+                        </div>
 
+                        <div className='relative mb-6'>
+                            <label htmlFor='password' className='block text-sm font-medium text-gray-700'>Password</label>
+                            <FontAwesomeIcon icon={faKey} className='absolute top-9.5 left-2 text-md text-gray-400' />
+                            <div className='absolute top-8.5 left-8 h-6 border-l border-gray-400'></div>  
+                            <input type={showPassword ? "password" : "text"} 
+                            name='password' 
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className={`relative w-full px-10 py-2 mt-1 border border-gray-300 rounded-md shadow-sm  ${error ? 'border-red-600' : ''} focus:outline-none focus:ring-zuccini-900 focus:border-zuccini-900`}/>
+                            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} 
+                            onClick={() => {setShowPassword((current) => !current)}}
+                            className='absolute cursor-pointer top-9 right-3 text-neutral-400'/>
+                            {error && <span className='text-[11px] text-red-600'>{error}</span>}
+                        </div>
+                    
                     <a href='#' className='mb-2 font-light text-blue-600'>Forgot your password?</a>
 
-                    <Link to="/Dashboard">
+                    
                         <button type='submit' className='w-full py-2 text-white transition duration-200 rounded-md cursor-pointer bg-zuccini-700 hover:bg-zuccini-800'>Login</button>
-                    </Link>
+                    
                 </form>
             </div>
         </div>
@@ -64,10 +114,5 @@ const Login = () => {
     </>
     )
 }
-
-
-
-
-
 
 export default Login;
